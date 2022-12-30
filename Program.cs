6d2df8c3b46a65d33ed6ccf969380cb42e9427d7
mor.loivothanh <quocbinhgithub@gmail.com>
2022-12-30 13:54:29 +0700

    [add] bien kieu du lieu trong C sharp

--- Program.cs
+++ Program.cs
@@ -1,13 +1,41 @@
-﻿namespace XuanThuLab
+﻿using System;
+namespace XuanThuLab
 {
     class Program{
-        static  void Main(string[] args){
-        Console.WriteLine("What is your name?");
-        var name = Console.ReadLine();
-        var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-        Console.Write($"{Environment.NewLine}Press any key to exit...");
-        Console.ReadKey(true);
+        static void Main(string[] args){
+            string name;
+            char kitu;
+            kitu = 'a'; // ki tu de trong ngoac don
+            name = "binh"; // chuoi de trong ngoac kep
+            string age = "binh";
+            if(age == name) {
+                Console.WriteLine("Bang nhau");
+            }else{
+               Console.WriteLine("Ko Bang nhau");
+            }
+            const double PI = 3.14; // constants
+            Console.WriteLine("Dy laf Hang so PI : {0}", PI);
+            float soFloat;
+            soFloat = 12.12f;
+            object student;
+            student = name;
+            Console.Title ="Vi du ve doi tuong COnsole";
+            Console.WriteLine("name : {0}", name);
+            Console.WriteLine("soFloat : {0}", soFloat);
+            Console.WriteLine("student : {0}", student);
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("soFloat : {0}", soFloat);
+            Console.Write("Vui long Nhan phim Enter");
+            if ( Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+            Console.Write("Vui long nhap ten cua ban");
+            string? hovaten;
+            hovaten = Console.ReadLine();
+            Console.WriteLine($"xin chao {hovaten}");
+            }
+
+          
+          //  Console.Clear();
         }
     }
 }
