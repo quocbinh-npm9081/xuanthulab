42883ccf32ada8d113481d6c9e823946359b3fa9
mor.loivothanh <quocbinhgithub@gmail.com>
2022-12-30 17:26:00 +0700

    [add] add struct and enum

--- Program.cs
+++ Program.cs
@@ -1,13 +1,64 @@
 ﻿namespace XuanThuLab
 {
     class Program{
-        static  void Main(string[] args){
-        Console.WriteLine("What is your name?");
-        var name = Console.ReadLine();
-        var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-        Console.Write($"{Environment.NewLine}Press any key to exit...");
-        Console.ReadKey(true);
+        //SỰ KHÁC BIỆT CỦA STRUC LÀ THAM TRỊ - CÒN CLASS LÀ THAM CHIẾU
+        public struct Product{ // struct la tham tri ( luu o stack)
+
+            //du lieu
+            public string name;
+            public double price {set;get;}
+            
+            //phuong thuoc
+            public string getInfo(){
+                return $"Ten san pham: {name} - gia: {price}";
+            }
+            //ham tao
+            public Product(string _name, double _price){
+                this.name= _name;
+                this.price = _price;
+            }           
+        }
+          // ENUM
+        //PHÂN LOẠI HỌC LỰC
+        enum TYPEHOCLUC {Kem, TrungBinh, Kha, Gioi}
+    
+        static  void Main(string[] args){      
+
+
+  
+          
+            Product sanpham1 = new Product("samsung", 1000);
+            Product sanpham2 = new Product("samsung", 1000);
+            if(sanpham1.Equals(sanpham2)){
+                Console.WriteLine("stsanpham1 Bang sanpham2");
+            }else{
+                Console.WriteLine("stsanpham1 Khong bang sanpham2");
+
+            }
+            Console.WriteLine(sanpham1.getInfo());
+            Console.WriteLine(sanpham1.price);
+            TYPEHOCLUC hocluc;
+            hocluc = TYPEHOCLUC.Kha; 
+            int soHocLuc = (int)hocluc; // tim index trong enum
+            Console.WriteLine(soHocLuc);
+            switch (hocluc)
+            {
+                case TYPEHOCLUC.Kem :
+                Console.WriteLine("Hoc luc Kem");
+                break;
+                case TYPEHOCLUC.Kha :
+                Console.WriteLine("Hoc luc Kha");
+                break;
+                case TYPEHOCLUC.TrungBinh :
+                Console.WriteLine("Hoc luc TrungBinh");
+                break;
+                case TYPEHOCLUC.Gioi :
+                Console.WriteLine("Hoc luc Gioi");
+                break;
+                default:
+                    return;
+            }
+
         }
     }
 }
