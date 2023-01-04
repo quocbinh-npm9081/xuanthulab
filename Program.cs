22770da6bb582999f2cc6b4985df922d7727b642
louis <quocbinhgithub@gmail.com>
2023-01-04 20:32:55 +0700

    [add] add null and nulable

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿namespace XuanThuLab
 {
+    //null và nullable
+    //null là không tham chiếu đến vùng nhớ nào cả 
     class Program{
+
         static  void Main(string[] args){
-        Console.WriteLine("What is your name?");
-        var name = Console.ReadLine();
-        var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-        Console.Write($"{Environment.NewLine}Press any key to exit...");
-        Console.ReadKey(true);
+            int? a;// trong C# kiểu dữ liệu nguyên thủy( là kiểu dữ liệu tham trị được lưu trên Stack không được phép null, nếu muốn null thì phải có toán tử int? )
+            a = null;
+            a = 10;
+            if(a.HasValue){//proprety HasValue để kiểm tra xem biến a có tham chiếu đến ô nhớ nào hay không( HasValue tương tự a != null)
+                Console.WriteLine($"bien a: {a.Value}");
+            }
+
         }
     }
 }
